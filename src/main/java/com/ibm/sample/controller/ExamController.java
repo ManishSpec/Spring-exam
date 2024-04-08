09d91bc33505700fc57df6e538c5d7c4cf566d6d
Spec Student <specstudent689@gmail.com>
2024-04-08 15:19:16 +0530

    results controller added

--- src/main/java/com/ibm/sample/controller/ExamController.java
+++ src/main/java/com/ibm/sample/controller/ExamController.java
@@ -3,11 +3,14 @@ package com.ibm.sample.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ibm.sample.entity.StudentMarks;
+import com.ibm.sample.model.StudentResult;
 import com.ibm.sample.service.ExamService;
 
 @RestController
@@ -23,5 +26,13 @@ public class ExamController {
 		
 		return examService.allMarksToDB(marksList);
 	}
+	
+	@GetMapping("/exam/marks/res/{rollno}")
+	public StudentResult  getResult(@PathVariable int rollno){
+		
+		return examService.getResults(rollno);
+	}
+	
+	
 
 }
--- src/main/java/com/ibm/sample/service/ExamService.java
+++ src/main/java/com/ibm/sample/service/ExamService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ibm.sample.entity.StudentMarks;
+import com.ibm.sample.model.StudentResult;
 import com.ibm.sample.repo.StudentMarksRepo;
 
 @Service
@@ -18,6 +19,52 @@ public class ExamService {
 	public List<StudentMarks> allMarksToDB(List<StudentMarks> marksList) {
 		
 		return marksRepo.saveAll(marksList);
+	}
+
+
+	public StudentResult getResults(int rollno) {
+		// TODO Auto-generated method stub
+		
+		StudentMarks stuMarks = marksRepo.findById(rollno).orElse(new StudentMarks());
+		
+		StudentResult stuRes = new StudentResult(stuMarks);
+		int count =0;
+		
+		if( (stuMarks != null) &&(stuMarks.getRollno() == rollno)) {
+			
+			if(stuMarks.getMaths() > 35) {
+				stuRes.setMathsRes("Pass");
+				count++;
+			}
+			if(stuMarks.getSci() > 35) {
+				stuRes.setSciRes("Pass");
+				count++;
+			}
+			if(stuMarks.getSco() > 35) {
+				stuRes.setScoRes("Pass");
+				count++;
+			}
+			if(stuMarks.getEng() > 35) {
+				stuRes.setEngRes("Pass");
+				count++;
+			}
+			if(stuMarks.getHindi() > 35) {
+				stuRes.setHindiRes("Pass");
+				count++;
+			}
+			
+			if(count == 5) {
+				stuRes.setFinalRes("Pass");
+			}
+			else {
+				stuRes.setFinalRes("Fail");
+			}
+			
+			
+		}
+		
+		
+		return stuRes;
 	}
 
 }
